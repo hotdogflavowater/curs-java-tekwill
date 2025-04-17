1a15d1fc447ef83edd3108ade377d993ad351da7
hotdog <stastropsa@gmail.com>
2025-04-17 14:24:17 +0300

    Homework #1

---
+++ src/main/java/homework1/HelloJavaWorld.java
@@ -0,0 +1,9 @@
+package homework1;
+
+public class HelloJavaWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java World!");
+
+    }
+}
---
+++ src/main/java/homework1/Variables.java
@@ -0,0 +1,20 @@
+package homework1;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        String FirstLetterInAlphabet = "A";
+        Boolean JavaIsAmazing = true;
+        double Pi = 3.14159;
+
+        System.out.println(
+                        "Current World Population: " + CurrentWorldPopulation + "\n" +
+                        "Cars Produced This Year: " + CarsProducedThisYear + "\n" +
+                        "First Letter In Alphabet: " + FirstLetterInAlphabet + "\n" +
+                        "Java Is Amazing: " + JavaIsAmazing + "\n" +
+                        "Pi: " + Pi + "\n"
+        );
+    }
+}
