39b7c6cef30dd9452197655f197ff47d8fb3a6d1
hotdog <stastropsa@gmail.com>
2025-04-20 17:44:06 +0300

    homeWork2

---
+++ src/main/java/edu/tekwill/academy/computer/computer.java
@@ -0,0 +1,19 @@
+package edu.tekwill.academy.computer;
+
+import edu.tekwill.academy.computer.components.processor;
+import edu.tekwill.academy.computer.components.hardDrive;
+import edu.tekwill.academy.computer.components.motherboard;
+import edu.tekwill.academy.computer.peripherals.keyboard;
+import edu.tekwill.academy.computer.peripherals.monitor;
+import edu.tekwill.academy.computer.peripherals.mouse;
+
+
+public class computer {
+    private processor processor;
+    private hardDrive hardDrive;
+    private motherboard motherboard;
+
+    private monitor monitor;
+    private keyboard keyboard;
+    private mouse mouse;
+}
